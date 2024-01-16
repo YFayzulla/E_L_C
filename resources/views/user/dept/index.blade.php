8b0ec0abcae92ffb232f082e62b21ce610b22a45
FayzullaYoldashev <yoldashevfayzulla0@gmail.com>
2024-01-16 22:55:47 +0500

    dept table

---
+++ resources/views/user/dept/index.blade.php
@@ -0,0 +1,18 @@
+@extends('template.master')
+@section('content')
+
+    <div class="p-4 m-4 sm:p-8 bg-white shadow sm:rounded-lg ">
+
+
+        <div class="max-w-xl">
+            <table class="table">
+                <tr>
+                    <td>No</td>
+                    <td>ismi</td>
+                    <td>status</td>
+                    <td>pul to`lash</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+@endsection

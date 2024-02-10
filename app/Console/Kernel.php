41367433aa0259cd70a28a6ef7ea4165d87d5e8f
Fayzulla <yoldashevfayzulla0@gmail.com>
2024-02-10 18:36:05 +0500

    test success

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 //        $schedule->command('sms:send')->everyMinute();
-        $schedule->command('user:status:update')->everyMinute();
+        $schedule->command('user:status:update')->monthly();
         //        $schedule->command('sms:send')->everyMinute();
     }
 

733ccecf74bf5b73d3ec06c29d01519ccb0c6008
YoldashevFayzulla <yoldashevfayzulla0@gmail.com>
2024-02-12 19:32:31 +0500

    rout fixed

--- routes/web.php
+++ routes/web.php
@@ -25,16 +25,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
 Route::get('/1', function () {
     $student=\App\Models\User::find(3);
     return view('user.pdf.student_show',compact('student'));
 });
-//profil
+
+ //profil
+
 Route::middleware('auth')->group(function () {
-    Route::get('dashboard', [Controller::class, 'auth'])->name('user');
+    Route::get('/', [Controller::class, 'auth'])->name('user');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');

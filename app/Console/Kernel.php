1e122d1f7faad606ac50936965593e4bd16681a3
YoldashevFayzulla <yoldashevfayzulla0@gmail.com>
2024-03-04 16:32:45 +0500

    cron job every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,8 +17,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         //        $schedule->command('sms:send')->everyMinute();
-        $schedule->command('user:status:update')->monthlyOn(1,'7:00');
-        //        $schedule->command('user:status:update')->everyMinute();
+//        $schedule->command('user:status:update')->monthlyOn(1,'7:00');
+                $schedule->command('user:status:update')->everyMinute();
         //        $schedule->command('sms:send')->everyMinute();
     }
 

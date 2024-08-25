62d3c5428eda11bee4ed03557570601fbe9ac0ef
YFayzulla <yoldashevFayzulla0@gmail.com>
2024-08-25 23:05:52 +0500

    main migrating finished changing the project with another

--- database/migrations/2024_08_22_161815_change_get_mark_column_type.php
+++ database/migrations/2024_08_22_161815_change_get_mark_column_type.php
@@ -1,11 +1,10 @@
-<?php
+`<?php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -14,17 +13,21 @@ return new class extends Migration
     public function up()
     {
         Schema::table('assessments', function (Blueprint $table) {
-            // Convert the column type to integer without losing data
-            $table->integer('get_mark')->change();
+            $table->integer('new_get_mark')->nullable();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
+        // Revert the column type using raw SQL if necessary
         Schema::table('assessments', function (Blueprint $table) {
-            // Revert back to the original type if needed
-            $table->string('get_mark')->change();
+            $table->string('new_get_mark')->nullable();
         });
     }
-
 };
+

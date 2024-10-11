dd194ef29e28615a2d58089098634990bff27ecd
YFayzulla <yoldashevFayzulla0@gmail.com>
2024-10-11 21:42:21 +0500

    alert

--- app/Http/Controllers/RefreshController.php
+++ app/Http/Controllers/RefreshController.php
@@ -9,7 +9,9 @@ class RefreshController extends Controller
 {
     public function update($id)
     {
+
         User::query()->findOrFail($id)->update(['status' => 0]);
         return redirect()->back()->with('success','data updated successfully');
+
     }
 }

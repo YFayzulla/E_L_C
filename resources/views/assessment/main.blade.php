1280787cac6942afbc08d5b1f13de6847f87b072
YFayzulla <yoldashevfayzulla0@gmail.com>
2024-10-23 17:38:37 +0500

    style changed in assessment

--- resources/views/assessment/main.blade.php
+++ resources/views/assessment/main.blade.php
@@ -1,9 +1,7 @@
 @extends('template.master')
 @section('content')
-
-    <div class="container-xxl flex-grow-1 container-p-y">
+    <div class="p-4 m-4 sm:p-8 bg-white shadow sm:rounded-lg ">
         <div class="row">
-            <div class="col-md-6 mt-2">
                 <div class="card">
                     <h5 class="card-header">Top 5 Students</h5>
                     <div class="table-responsive text-nowrap">
@@ -28,41 +26,43 @@
                     </div>
                 </div>
             </div>
-            <!-- Second Table -->
-            <div class="col-md-6 mt-2">
-                <div class="card">
-                    <h5 class="card-header">Test List</h5>
-                    <div class="table-responsive text-nowrap">
-                        <table class="table">
-                            <thead>
-                            <tr>
-                                <td>Name</td>
-                                <td>Group</td>
-                                <td>Data</td>
-                                <td>show</td>
-                            </tr>
-                            </thead>
-                            <tbody class="table-border-bottom-0">
 
-                            @foreach($data as $item)
-                                <tr>
-                                    <td>{{$item->name}}</td>
-                                    <td>{{$item->groupName->name ?? "no data"}}</td>
-                                    <td>{{$item->created_at->format('D-M-Y')}}</td>
-                                    <td>
-                                        <a href="{{route('test.show', $item->id)}}" class="btn btn-info"><i
-                                                class="bx bx-show-alt"></i></a>
-                                    </td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                        <div class="card-footer">
-                            {{ $data->links('pagination::bootstrap-5') }}
-                        </div>
+        </div>
+    <div class="p-4 m-4 sm:p-8 bg-white shadow sm:rounded-lg ">
+
+
+        <!-- Second Table -->
+            <div class="card">
+                <h5 class="card-header">Test List</h5>
+                <div class="table-responsive text-nowrap">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <td>Name</td>
+                            <td>Group</td>
+                            <td>Data</td>
+                            <td>show</td>
+                        </tr>
+                        </thead>
+                        <tbody class="table-border-bottom-0">
+
+                        @foreach($data as $item)
+                            <tr>
+                                <td>{{$item->name}}</td>
+                                <td>{{$item->groupName->name ?? "no data"}}</td>
+                                <td>{{$item->created_at->format('D-M-Y')}}</td>
+                                <td>
+                                    <a href="{{route('test.show', $item->id)}}" class="btn btn-info"><i
+                                            class="bx bx-show-alt"></i></a>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                    <div class="card-footer">
+                        {{ $data->links('pagination::bootstrap-5') }}
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 @endsection

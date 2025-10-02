1f82e864071624206724778b9a7212e3e41e59ef
YFayzulla <yoldashevfayzulla0@gmail.com>
2025-10-02 15:54:22 +0500

    cron changed

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         //        $schedule->command('sms:send')->everyMinute();
 //        $schedule->command('user:status:update')->monthlyOn(1,'7:00');
-                $schedule->command('user:status:update')->everyMinute();
+                $schedule->command('user:status:update')->everyMonth();
         //        $schedule->command('sms:send')->everyMinute();
     }
 
